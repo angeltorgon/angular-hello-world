f6c053c3971ed5db82f0a7b2b46cbb7101f3b728
angel-torres <alas4224@gmail.com>
2019-05-27 23:15:56 -0500

    create two components; render date component on app component

---
+++ first-project/src/app/date/date.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'date',
+  templateUrl: './date.component.html',
+  styleUrls: ['./date.component.css']
+})
+export class DateComponent implements OnInit {
+
+  constructor() { }
+
+  message: string = new Date().toDateString();
+
+  ngOnInit() {
+  }
+
+}
