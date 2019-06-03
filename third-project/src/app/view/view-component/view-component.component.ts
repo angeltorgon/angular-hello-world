31c92912c16f60b176ef4ad9b924f5f1a26ab901
angel-torres <alas4224@gmail.com>
2019-06-03 18:14:42 -0500

    import new module into app module

---
+++ third-project/src/app/view/view-component/view-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-component',
+  templateUrl: './view-component.component.html',
+  styleUrls: ['./view-component.component.css']
+})
+export class ViewComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
