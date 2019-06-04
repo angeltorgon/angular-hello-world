dfe2f7e029480d5ec6576153d5037d0f6b546c22
angel-torres <alas4224@gmail.com>
2019-06-04 10:01:49 -0500

    Was able to use http client module to make http request

--- third-project/src/app/app.component.ts
+++ third-project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TestService } from './test.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,9 @@ import { TestService } from './test.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(svc: TestService) {
+  constructor(private svc: TestService, private http: HttpClient) {
     svc.log('Service injection worked!')
+    const observable = this.http.get("https://rickandmortyapi.com/api/character/14");
+    observable.subscribe( res => console.log(res));
   }
 }
